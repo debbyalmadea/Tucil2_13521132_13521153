9eda960f6c1fabc5e7154f866f78076e4f0db4b9
debbyalmadea <almadeaputri@gmail.com>
2023-02-28 19:56:15 +0700

    fix: points, add: comments

--- src/lib/linalg.py
+++ src/lib/linalg.py
@@ -1,15 +1,19 @@
-import points.point as p
 import math
 
+# menghitung pemanggilan fungsi norm
 func_called = 0
 
 
 def norm(point1, point2):
+    """
+    mengembalikan jarak euclidean 
+    dari point1 dan point2
+    """
     global func_called
     func_called += 1
 
-    _norm = 0
+    norm = 0
     for i in range(point1.dimension):
-        _norm += math.pow(point1.get(i) - point2.get(i), 2)
+        norm += math.pow(point1.get(i) - point2.get(i), 2)
 
-    return math.sqrt(_norm)
+    return math.sqrt(norm)
